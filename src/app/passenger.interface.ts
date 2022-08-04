01875dd7c5c76cf8c51f96ef629e5da5cc633af7
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-08-03 20:26:55 -0500

    Modify Passenger interface

--- src/app/passenger.interface.ts
+++ src/app/passenger.interface.ts
@@ -1,7 +1,11 @@
+import { Child } from "./child.interface";
+
 export interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
   //checkInDate?: number | null;
   checkInDate: number | null;
+  //children?: Child[] | null;
+  children: Child[] | null;
 }
