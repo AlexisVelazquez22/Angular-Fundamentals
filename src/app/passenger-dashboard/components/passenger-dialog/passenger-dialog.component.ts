c2753292ed68efa1d740d5c29fc322c0569fdc04
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-08-12 16:20:39 -0500

    Create dialog component

---
+++ src/app/passenger-dashboard/components/passenger-dialog/passenger-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-dialog',
+  styleUrls: ['passenger-dialog.component.scss'],
+  template: `
+    <div>
+      Hello dialog
+    </div>
+  `
+})
+export class PassengerDialog {
+
+}
